845a05d27bd39ea2abe7cd264aabf5fbb5d07182
taulant.disha <taulantdisha@Taulants-MacBook-Pro.local>
2022-07-03 14:44:46 +0300

    changed icons

--- src/common/data/index.js
+++ src/common/data/index.js
@@ -6,23 +6,28 @@ import footerDiscord from "common/assets/image/discord-social.svg";
 import footerGithub from "common/assets/image/github-social.svg";
 import footerTwitter from "common/assets/image/twitter-social.svg";
 import footerMedium from "common/assets/image/medium-social.svg";
+import footerDocs from "common/assets/image/docs.svg";
 
 export const FooterData = {
   menu: [],
   logo: footerLogo,
   copyright: `Copyright © ${new Date().getFullYear()}, Aut.`,
   social: [
+    // {
+    //   link: "https://github.com/Aut-Protocol",
+    //   icon: footerGithub,
+    // },
     {
-      link: "https://discord.gg/jntyjSvFZd",
-      icon: footerDiscord,
-    },
-    {
-      link: "https://github.com/Aut-Protocol",
-      icon: footerGithub,
+      link: "https://docs.aut.id",
+      icon: footerDocs,
     },
     {
       link: "https://twitter.com/opt_aut",
       icon: footerTwitter,
+    },
+    {
+      link: "https://discord.gg/jntyjSvFZd",
+      icon: footerDiscord,
     },
     // {
     //   link: "https://blog.skillwallet.id",
